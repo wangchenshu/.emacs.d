4c8a5ea439ea801abc7666f4b72507af5765002c
Walter Wang <csw.dawm@gmail.com>
2016-06-02 21:21:18 +0800

    update

---
+++ elpa/wttrin-20160306.507/wttrin-pkg.el
@@ -0,0 +1,1 @@
+(define-package "wttrin" "20160306.507" "Emacs frontend for weather web service wttr.in" '((emacs "24.4") (xterm-color "1.0")) :url "https://github.com/bcbcarl/emacs-wttrin" :keywords '("comm" "weather" "wttrin"))
